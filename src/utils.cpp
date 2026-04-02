0b53a625a2d2b31c43574e43e19c1fc4eb46096f
YogSotho <monnocellula@gmail.com>
2026-04-03 00:47:43 +0200

    Create utils.cpp

---
+++ src/utils.cpp
@@ -0,0 +1,25 @@
+#include "utils.hpp"
+#include <yaml-cpp/yaml.h>
+#include <stdexcept>
+
+Config load_config(const std::string& filename) {
+    YAML::Node root = YAML::LoadFile(filename);
+    Config cfg;
+    cfg.model_path = root["model_path"].as<std::string>();
+    cfg.vocab_path = root["vocab_path"].as<std::string>();
+    if (root["merges_path"]) cfg.merges_path = root["merges_path"].as<std::string>();
+    if (root["listen_addr"]) cfg.listen_addr = root["listen_addr"].as<std::string>();
+    if (root["listen_port"]) cfg.listen_port = root["listen_port"].as<int>();
+    if (root["enable_tls"]) cfg.enable_tls = root["enable_tls"].as<bool>();
+    if (root["cert_file"]) cfg.cert_file = root["cert_file"].as<std::string>();
+    if (root["key_file"]) cfg.key_file = root["key_file"].as<std::string>();
+    if (root["api_key"]) cfg.api_key = root["api_key"].as<std::string>();
+    if (root["n_threads"]) cfg.n_threads = root["n_threads"].as<int>();
+    if (root["max_context"]) cfg.max_context = root["max_context"].as<size_t>();
+    if (root["temperature_default"]) cfg.temperature_default = root["temperature_default"].as<float>();
+    if (root["top_p_default"]) cfg.top_p_default = root["top_p_default"].as<float>();
+    if (root["max_new_tokens_default"]) cfg.max_new_tokens_default = root["max_new_tokens_default"].as<int>();
+    if (root["memory_limit_bytes"]) cfg.memory_limit_bytes = root["memory_limit_bytes"].as<size_t>();
+    if (root["sandbox_enabled"]) cfg.sandbox_enabled = root["sandbox_enabled"].as<bool>();
+    return cfg;
+}
